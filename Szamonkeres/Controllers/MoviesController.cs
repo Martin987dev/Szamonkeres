737491a24f75342b6cc9d5ace7b3f026e07ace78
Martin987dev <lempnerm@kkszki.hu>
2026-01-12 09:30:54 +0100

    Feladat 3

--- Szamonkeres/Controllers/MoviesController.cs
+++ Szamonkeres/Controllers/MoviesController.cs
@@ -1,6 +1,41 @@
-﻿namespace Szamonkeres.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Szamonkeres.Models;
+
+namespace Szamonkeres.Controllers
 {
-    public class MoviesController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
     {
+        private readonly CinemadbContext _context;
+        public MoviesController(CinemadbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("10.Feladat")]
+        public ActionResult GetMovies()
+        {
+            try
+            {
+                var movieList = _context.Movies
+                    .Select(movie => new
+                    {
+                        movie.MovieId,
+                        movie.Title,
+                        movie.ReleaseDate,
+                        movie.ActorId,
+                        movie.FilmTypeId
+                    })
+                    .ToList();
+
+                return Ok(movieList);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(new { error = e.Message });
+            }
+        }
+
     }
 }
